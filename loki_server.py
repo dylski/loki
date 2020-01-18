fc367a7dd0975fbedc73bd3e0ed87c85b81731a3
Dylan Banarse <dylski@google.com>
2020-01-18 16:53:28 +0000

    Licence details

--- loki_server.py
+++ loki_server.py
@@ -1,5 +1,21 @@
+"""
+Loki - Lock and key-based artificial-life simulation generating pretty patterns.
+Copyright (C) 2019 Dylan Banarse
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from flask import Flask, render_template, Response
-# Raspberry Pi camera module (requires picamera package, developed by Miguel Grinberg)
 import loki1Dv as loki
 
 app = Flask(__name__)
--- test_loki.py
+++ test_loki.py
@@ -1,3 +1,20 @@
+"""
+Loki - Lock and key-based artificial-life simulation generating pretty patterns.
+Copyright (C) 2019 Dylan Banarse
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from loki1Dv import Key, State, Loki, mutate_array
 import numpy as np
 import unittest
